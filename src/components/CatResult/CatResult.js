713c99ad46b9ba33abe4a7bcdc6549451452fe97
Zahid <darzahid537@gmail.com>
2022-03-17 18:41:04 +0530

    on progress

--- src/components/CatResult/CatResult.js
+++ src/components/CatResult/CatResult.js
@@ -53,7 +53,7 @@ function CatResult() {
     <>
       <Header />{" "}
       <HeaderFooterWrapper>
-        <div className="pt-2  background-tertiary h-screen  mb-4 relative">
+        <div className="pt-2   background-tertiary h-screen  mb-4 relative ion-padding">
           <div className="flex items-center justify-center">
             <span>{catResult.category_name}</span>
           </div>
@@ -61,48 +61,51 @@ function CatResult() {
             catResult.productlist &&
             catResult.productlist.map((item, key) => (
               <>
-                <div className="flex items-center justify-center ">
-                  <span className="text-slate-600 font-w-500">
-                    {item.category_name}
-                  </span>
-                </div>
-                <div key={key} className="ion-padding  ">
-                  <div className="bg-white  flex rounded-lg">
-                    <img
-                      style={{ width: "27%" }}
-                      src={`${config.baseUrl}/${item.product_image}`}
-                      alt=""
-                    />
-                    <div
-                      className="absolute "
-                      style={{ left: "0", left: "16px" }}
-                    >
-                      <span className="inline-block leading-3	text-xs px-3 py-2 background-primary rounded-r-full rounded-b-full">
+                <div className="flex bg-white mb-3 h-24 relative">
+                  <div
+                    className="p-2 flex justify-center items-center h-full"
+                    style={{
+                      width: "120px",
+                    }}
+                  >
+                    <div>
+                      <span className="inline-block leading-3	text-xs px-3 py-2 background-primary absolute top-0 left-0 rounded-r-full rounded-b-full">
                         {item.product_info[0].product_discount}%
                         <br />
                         OFF
                       </span>
                     </div>
-                    <div className="ml-2 py-4">
-                      <h1 className="font13 font-w-600">{item.product_name}</h1>
-                      <span className="font13 font-w-600">
+                    <img
+                      className="inline-block h-full"
+                      src={
+                        item.product_image[0]
+                          ? `${config.baseUrl}/${item.product_image[0]}`
+                          : "https://img.icons8.com/ios/70/000000/no-camera--v1.png"
+                      }
+                    />
+                  </div>
+                  <div className="grow py-2">
+                    <p>{item.product_name}</p>
+                    <div>
+                      <span className="font12 font-semibold pl-2">
                         Rs
                         {parseFloat(
                           item.product_info[0].product_price -
                             (item.product_info[0].product_price / 100) *
                               item.product_info[0].product_discount
-                        ).toFixed(2)}{" "}
+                        ).toFixed(2)}
                       </span>
-                      <span className="font13 font-w-600 ml-1 text-slate-500">
+                      &nbsp;
+                      <span className="font10 font-semibold line-through text-light-grey">
+                        Rs
                         {parseFloat(item.product_info[0].product_price).toFixed(
                           2
                         )}
                       </span>
                     </div>
-                    <div className="pt-12">
-                      <button className="background-primary font13 font-w-500 py-1 px-5 ml-2">
-                        Add To Cart
-                      </button>
+                    {/* <CustomDropDown /> */}
+                    <div className="absolute background-primary bottom-0 right-0 py-1 px-5">
+                      <button className="font15 font-w-500">Add To Cart</button>
                     </div>
                   </div>
                 </div>
