53d8293f77b73ade46261c0383867c42c87be2d8
Saleem-un-qayoom <saleemunqayoom@gmail.com>
2022-03-25 15:34:03 +0530

    changes

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,70 +1,69 @@
-import React, { useEffect, useState } from "react";
-import SimpleImageSlider from "react-simple-image-slider";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { bannersAtom } from "../../Recoil/atom";
-import config from "../../Services/config";
+import 'swiper/css/pagination';
+import 'swiper/css';
 
-import "swiper/css/pagination";
-import "swiper/css";
+import React, { useEffect, useState } from 'react';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
-import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
-
-import { Pagination } from "swiper";
+import { Pagination } from 'swiper';
+import SimpleImageSlider from 'react-simple-image-slider';
+import { bannersAtom } from '../../Recoil/atom';
+import config from '../../Services/config';
 
 const images = [
-  {
-    url: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
-  },
-  {
-    url: "https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__340.jpg",
-  },
-  {
-    url: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
-  },
+	{
+		url: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+	},
+	{
+		url: 'https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__340.jpg',
+	},
+	{
+		url: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+	},
 ];
 
 function Slider() {
-  const banner = useRecoilValue(bannersAtom);
+	const banner = useRecoilValue(bannersAtom);
 
-  return (
-    // <div className="w-full ">
-    //   <SimpleImageSlider
-    //     width={"100%"}
-    //     height={"150px"}
-    //     style={
-    //       {
-    //         // backgroundSize: "cover",
-    //         // backgroundPosition: "center center",
-    //       }
-    //     }
-    //     images={banner.map((item) => `${config.baseUrl}/${item.img}`)}
-    //     showBullets={false}
-    //     showNavs={false}
-    //     autoPlay
-    //   />
-    // </div>
-    <Swiper
-      // modules={[Pagination]}
-      // spaceBetween={50}
-      // navigation
-      // pagination={{ clickable: true }}
-      slidesPerView={1}
-      scrollbar={{ draggable: true }}
-      Autoplay
-    >
-      {banner.map((item) => (
-        <SwiperSlide>
-          <div
-            className="w-screen h-40 bg-cover bg-no-repeat"
-            style={{
-              backgroundImage: `url(${config.baseUrl}/${item.img})`,
-              backgroundPosition: "0 center",
-            }}
-          ></div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  );
+	return (
+		// <div className="w-full ">
+		//   <SimpleImageSlider
+		//     width={"100%"}
+		//     height={"150px"}
+		//     style={
+		//       {
+		//         // backgroundSize: "cover",
+		//         // backgroundPosition: "center center",
+		//       }
+		//     }
+		//     images={banner.map((item) => `${config.baseUrl}/${item.img}`)}
+		//     showBullets={false}
+		//     showNavs={false}
+		//     autoPlay
+		//   />
+		// </div>
+		<Swiper
+			// modules={[Pagination]}
+			// spaceBetween={50}
+			// navigation
+			// pagination={{ clickable: true }}
+			slidesPerView={1}
+			scrollbar={{ draggable: true }}
+			Autoplay
+		>
+			{banner.map(item => (
+				<SwiperSlide>
+					<div
+						className="w-screen h-40 bg-cover bg-no-repeat"
+						style={{
+							backgroundImage: `url(${config.baseUrl}/${item.img})`,
+							backgroundPosition: '0 center',
+						}}
+					></div>
+				</SwiperSlide>
+			))}
+		</Swiper>
+	);
 }
 
 export default Slider;
