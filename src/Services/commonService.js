543cb3d2a59174df1a1647b27bb56648b0f18fd8
Saleem-un-qayoom <saleemunqayoom@gmail.com>
2022-03-31 12:03:15 +0530

    added functionalyt to add remove item to cart

--- src/Services/commonService.js
+++ src/Services/commonService.js
@@ -1,13 +1,69 @@
-// import { toastAtom } from '../Recoil/atom';
-// import { useRecoilState } from 'recoil';
+import { CartAtom } from '../Recoil/atom';
 
-// const ShowToast = (text, setToast) => {
-// 	// const [toast, setToast] = useRecoilState(toastAtom);
-// 	setToast(text);
-// 	setTimeout(() => {
-// 		setToast('');
-// 	}, 3000);
-// 	return '';
-// };
+const commonService = {
+	addItemToCart: (item, cart, setCart) => {
+		let found = false;
+		let temp = [...cart];
+		for (let i = 0; i < temp.length; i++) {
+			if (temp[i].id === item.id) {
+				temp[i] = { ...temp[i], quantity: temp[i].quantity + 1 };
+				found = true;
+				break;
+			}
+		}
+		if (!found) {
+			temp.push({ ...item, quantity: 1 });
+		}
+		setCart(temp);
+		localStorage.setItem('pharm-box-cart', JSON.stringify(temp));
+	},
 
-// export default ShowToast;
+	increaseQuantity: (item, cart, setCart) => {
+		let temp = [...cart];
+		for (let i = 0; i < temp.length; i++) {
+			if (temp[i].id === item.id) {
+				temp[i] = { ...temp[i], quantity: temp[i].quantity + 1 };
+				break;
+			}
+		}
+
+		setCart(temp);
+		localStorage.setItem('pharm-box-cart', JSON.stringify(temp));
+	},
+
+	decreaseQuantity: (item, cart, setCart) => {
+		let lastItemInQuantity = -1;
+		let temp = [...cart];
+		for (let i = 0; i < temp.length; i++) {
+			if (temp[i].id === item.id) {
+				if (temp[i].quantity > 1) {
+					temp[i] = {
+						...temp[i],
+						quantity: temp[i].quantity - 1,
+					};
+				} else {
+					lastItemInQuantity = i;
+				}
+				break;
+			}
+		}
+		if (lastItemInQuantity !== -1) {
+			temp.splice(lastItemInQuantity, 1);
+		}
+		setCart(temp);
+		localStorage.setItem('pharm-box-cart', JSON.stringify(temp));
+	},
+	isItemAlreadyInCart: (item, cart, setCart) => {
+		let found = 0;
+		let temp = [...cart];
+		for (let i = 0; i < temp.length; i++) {
+			if (temp[i].id === item.id) {
+				found = temp[i].quantity;
+				break;
+			}
+		}
+		return found;
+	},
+};
+
+export default commonService;
--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,97 +1,27 @@
-import { useRecoilState } from "recoil";
-import { CartAtom, SelectedProductAtom } from "../../Recoil/atom";
-import config from "../../Services/config";
-import { navigate, useNavigate } from "react-router";
+import { CartAtom, SelectedProductAtom } from '../../Recoil/atom';
+import { navigate, useNavigate } from 'react-router';
+
+import ProductListItem from '../ProductListItem';
+import commonService from '../../Services/commonService';
+import config from '../../Services/config';
+import { useRecoilState } from 'recoil';
 
 function ProductList({ product }) {
-  const navigate = useNavigate();
+	return (
+		<div className="explore-something-new ion-padding bg-slate-50">
+			<div className="explore-something-new__head flex justify-between items-center  ">
+				<p className="text-xs font-medium text-color-primary ">
+					{product.title}
+				</p>
+			</div>
 
-  const [cart, setCart] = useRecoilState(CartAtom);
-  const [selectedProduct, setSelectedProduct] =
-    useRecoilState(SelectedProductAtom);
-
-  const addItemToCart = (item) => {
-    setCart([...cart, item]);
-    localStorage.setItem("pharm-box-cart", JSON.stringify([...cart, item]));
-  };
-
-  return (
-    <div className="explore-something-new ion-padding bg-slate-50">
-      <div className="explore-something-new__head flex justify-between items-center  ">
-        <p className="text-xs font-medium text-color-primary ">
-          {product.title}
-        </p>
-        {/* <button className="text-xs font-w-700">View all </button> */}
-      </div>
-
-      <div className=" flex mt-4 scrollable-element">
-        {product.product_data.map((item, key) => (
-          <div
-            key={key}
-            className="flex flex-col bg-white mr-3"
-            style={{
-              flex: "0 0 145px",
-            }}
-          >
-            <div
-              onClick={() => {
-                setSelectedProduct(item);
-                navigate("/product-description");
-              }}
-            >
-              <div>
-                <span className="inline-block leading-3	text-xs px-3 py-2 background-primary rounded-r-full rounded-b-full">
-                  {item.product_info[0].product_discount}%
-                  <br />
-                  OFF
-                </span>
-              </div>
-              <div className="h-28 flex items-center justify-center">
-                <img
-                  className="h-4/5"
-                  src={
-                    item.image
-                      ? `${config.baseUrl}/${item.image}`
-                      : "https://img.icons8.com/ios/70/000000/no-camera--v1.png"
-                  }
-                  alt=""
-                />
-              </div>
-              <div>
-                <span className="font12 font-semibold pl-2">
-                  Rs
-                  {parseFloat(
-                    item.product_info[0].product_price -
-                      (item.product_info[0].product_price / 100) *
-                        item.product_info[0].product_discount
-                  ).toFixed(2)}
-                </span>
-                &nbsp;
-                <span className="font10 font-semibold line-through text-light-grey">
-                  Rs
-                  {parseFloat(item.product_info[0].product_price).toFixed(2)}
-                </span>
-              </div>
-              <div>
-                <span className="font12 font-semibold truncate px-2">
-                  {item.product_name}
-                </span>
-              </div>
-            </div>
-
-            <div className="flex justify-center items-center background-primary w-full	 ">
-              <button
-                className="text-xs font-w-600 py-1 px3 w-full	"
-                onClick={() => addItemToCart(item)}
-              >
-                Add To Cart
-              </button>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+			<div className=" flex mt-4 scrollable-element">
+				{product.product_data.map((item, key) => (
+					<ProductListItem item={item} key={key} />
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default ProductList;
