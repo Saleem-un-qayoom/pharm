b3f39d086ec26724d08820d1266286320d0badae
Saleem-un-qayoom <saleemunqayoom@gmail.com>
2022-04-23 06:59:33 +0530

    fixed issue

--- src/View/Screens/TNCScreen/TNCScreen.js
+++ src/View/Screens/TNCScreen/TNCScreen.js
@@ -1,23 +1,23 @@
-import React from "react";
-import { useRecoilValue } from "recoil";
-import CommonScreenPage from "../../../components/CommonScreenPage/CommonScreenPage";
-import { tncAtom } from "../../../Recoil/atom";
+import CommonScreenPage from '../../../components/CommonScreenPage/CommonScreenPage';
+import React from 'react';
+import { tncAtom } from '../../../Recoil/atom';
+import { useRecoilValue } from 'recoil';
 
 function TermsandConditions() {
-  const termsAndConditionRecoil = useRecoilValue(tncAtom);
+	const termsAndConditionRecoil = useRecoilValue(tncAtom);
 
-  return (
-    <>
-      <CommonScreenPage
-        headingTitle={"Terms and Conditions"}
-      ></CommonScreenPage>
-
-      <div
-        className="ion-padding font11 font-w-700"
-        dangerouslySetInnerHTML={{ __html: termsAndConditionRecoil }}
-      />
-    </>
-  );
+	return (
+		<>
+			<CommonScreenPage headingTitle={'Terms and Conditions'}>
+				<div
+					className="ion-padding font11 font-w-700"
+					dangerouslySetInnerHTML={{
+						__html: termsAndConditionRecoil,
+					}}
+				/>
+			</CommonScreenPage>
+		</>
+	);
 }
 
 export default TermsandConditions;
