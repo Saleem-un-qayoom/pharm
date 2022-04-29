29260c83f541aabfdf49838e1ebdf39d8d815113
Saleem-un-qayoom <saleemunqayoom@gmail.com>
2022-04-29 19:56:55 +0530

    implemented auto check pincode

--- src/components/CommonScreenPage/CommonScreenPage.js
+++ src/components/CommonScreenPage/CommonScreenPage.js
@@ -1,102 +1,121 @@
-import "./CommonScreenPage.scss";
+import './CommonScreenPage.scss';
 
-import { CartAtom, userDataAtom } from "../../Recoil/atom";
-import React from "react";
-import { useNavigate } from "react-router";
-import { useRecoilValue } from "recoil";
+import { CartAtom, userDataAtom } from '../../Recoil/atom';
+
+import React from 'react';
+import { useNavigate } from 'react-router';
+import { useRecoilValue } from 'recoil';
 
 function CommonScreenPage({
-  headingTitle,
-  contentBg,
-  children,
-  showLoading = false,
-  showDeleteModal = false,
-  onDeleteModalClick,
-  showCart = false,
+	headingTitle,
+	contentBg,
+	children,
+	showLoading = false,
+	showDeleteModal = false,
+	onDeleteModalClick,
+	showCart = false,
+	hideBackButton = false,
 }) {
-  const navigate = useNavigate();
-  const cart = useRecoilValue(CartAtom);
-  const cartItems = [...cart];
-  const userData = useRecoilValue(userDataAtom);
+	const navigate = useNavigate();
+	const cart = useRecoilValue(CartAtom);
+	const cartItems = [...cart];
+	const userData = useRecoilValue(userDataAtom);
 
-  // const myOrderDetails = useRecoilValue(orderDetailsAtom);
+	// const myOrderDetails = useRecoilValue(orderDetailsAtom);
 
-  return (
-    <div className={`h-screen ${contentBg ? contentBg : "bg-white"}`}>
-      <div className="common-screen-page-header background-primary height-8 flex items-center ion-padding-x justify-between">
-        <div className="flex">
-          <img
-            onClick={() => navigate(-1)}
-            src="/assets/icons/back.png"
-            alt=""
-            className="h-5"
-          />
-          <p className="font-bold ml-2.5 capitalize">{headingTitle}</p>
-        </div>
-        {showCart && (
-          <div
-            className="relative ml-auto"
-            onClick={() => {
-              if (userData) {
-                navigate("/cart-page");
-              } else {
-                navigate("/login-page");
-              }
-            }}
-          >
-            <img className="h-7" src="/assets/icons/shopping-cart.svg" alt="" />
-            <span
-              style={{
-                top: -2,
-                right: -5,
-              }}
-              className="absolute  w-5 h-5 bg-gray-800 rounded-full text-white text-sm flex justify-center items-center"
-            >
-              {cartItems.length}
-            </span>
-          </div>
-        )}
-      </div>
-      <div className="common-screen-page-content height-92 scrollable-element-y ">
-        {children}
-      </div>
-      {showLoading && (
-        <div className="common-screen-page-loading h-screen w-screen absolute top-0 left-0 z-50 flex justify-center items-center">
-          <div className="w-56 h-14 bg-white rounded-md flex items-center px-1">
-            <img
-              className="w-11 inline-block"
-              src="/assets/icons/spinner-animated.svg"
-              alt=""
-            />
-            Loading...
-          </div>
-        </div>
-      )}
-      {showDeleteModal && (
-        <div className="common-screen-page-loading h-screen w-screen absolute top-0 left-0 z-50 flex justify-center items-center">
-          <div className="w-80  bg-white rounded-md px-1 ion-padding">
-            <div className="flex items-center">
-              <img
-                className="w-11 pr-3"
-                src="/assets/icons/delete.png"
-                alt=""
-              />
-              <span className="text-base">Delete</span>
-            </div>
-            <span className="text-sm pt-2 inline-block">
-              Are You Sure You Want to Delete?
-            </span>
-            <div className="flex justify-end text-sm pt-10">
-              <span className="pr-4" onClick={() => onDeleteModalClick(false)}>
-                Cancel
-              </span>
-              <span onClick={() => onDeleteModalClick(true)}>Delete</span>
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div className={`h-screen ${contentBg ? contentBg : 'bg-white'}`}>
+			<div className="common-screen-page-header background-primary height-8 flex items-center ion-padding-x justify-between">
+				<div className="flex">
+					{!hideBackButton && (
+						<img
+							onClick={() => navigate(-1)}
+							src="/assets/icons/back.png"
+							alt=""
+							className="h-5"
+						/>
+					)}
+					<p className="font-bold ml-2.5 capitalize">
+						{headingTitle}
+					</p>
+				</div>
+				{showCart && (
+					<div
+						className="relative ml-auto"
+						onClick={() => {
+							if (userData) {
+								navigate('/cart-page');
+							} else {
+								navigate('/login-page');
+							}
+						}}
+					>
+						<img
+							className="h-7"
+							src="/assets/icons/shopping-cart.svg"
+							alt=""
+						/>
+						<span
+							style={{
+								top: -2,
+								right: -5,
+							}}
+							className="absolute  w-5 h-5 bg-gray-800 rounded-full text-white text-sm flex justify-center items-center"
+						>
+							{cartItems.length}
+						</span>
+					</div>
+				)}
+			</div>
+			<div className="common-screen-page-content height-92 scrollable-element-y ">
+				{children}
+			</div>
+			{showLoading && (
+				<div className="common-screen-page-loading h-screen w-screen absolute top-0 left-0 z-50 flex justify-center items-center">
+					<div className="w-56 h-14 bg-white rounded-md flex items-center px-1">
+						<img
+							className="w-11 inline-block"
+							src="/assets/icons/spinner-animated.svg"
+							alt=""
+						/>
+						Loading...
+					</div>
+				</div>
+			)}
+			{showDeleteModal && (
+				<div className="common-screen-page-loading h-screen w-screen absolute top-0 left-0 z-50 flex justify-center items-center">
+					<div className="w-80  bg-white rounded-md px-1 ion-padding">
+						<div className="flex items-center">
+							<img
+								className="w-11 pr-3"
+								src="/assets/icons/delete.png"
+								alt=""
+							/>
+							<span className="text-base">Delete</span>
+						</div>
+						<span className="text-sm pt-2 inline-block">
+							Are You Sure You Want to Delete?
+						</span>
+						<div className="flex justify-end text-sm pt-10">
+							<span
+								className="pr-4"
+								onClick={() =>
+									onDeleteModalClick(false)
+								}
+							>
+								Cancel
+							</span>
+							<span
+								onClick={() => onDeleteModalClick(true)}
+							>
+								Delete
+							</span>
+						</div>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default CommonScreenPage;
